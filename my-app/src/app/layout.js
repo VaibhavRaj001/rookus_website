4c9f4099b527c6139222abfa6aedae688a4467af
VaibhavRaj001 <144055184+VaibhavRaj001@users.noreply.github.com>
2025-12-01 13:10:15 +0530

    meta-data

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -2,8 +2,8 @@
 import "./globals.css"; 
 
 export const metadata = {
-  title: "Rookus",
-  description: "Where AI meets Creativity",
+  title: "Rookus – AI Design Automation Platform",
+  description: "Rookus uses AI to generate designs, automate workflows, and boost your brand’s creativity.",
 };
 
 export default function RootLayout({ children }) {
