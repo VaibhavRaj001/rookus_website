b5276fd3b861d807e8ad53ef94236da8bc89d101
VaibhavRaj001 <144055184+VaibhavRaj001@users.noreply.github.com>
2026-01-18 22:20:37 +0530

    new-sitemap

--- my-app/src/app/sitemap.js
+++ my-app/src/app/sitemap.js
@@ -1,37 +1,12 @@
 export default function sitemap() {
   const baseUrl = "https://rookus.co.in";
-  const now = new Date();
 
   return [
     {
       url: baseUrl,
-      lastModified: now,
+      lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1.0,
-    },
-    {
-      url: `${baseUrl}/#features`,
-      lastModified: now,
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#pricing`,
-      lastModified: now,
-      changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/#research`,
-      lastModified: now,
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#sponsors`,
-      lastModified: now,
-      changeFrequency: "monthly",
-      priority: 0.7,
-    },
+    }
   ];
 }
